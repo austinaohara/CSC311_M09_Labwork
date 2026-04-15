cac1abc338af3603ec0e8720cc3e22941038c0f0
olaroso <o.aroso@yahoo.com>
2026-04-15 12:55:23 -0400

    Init commit

--- src/ex2/BlockingBuffer.java
+++ src/ex2/BlockingBuffer.java
@@ -1,47 +1,27 @@
 package ex2;
-// Fig. 23.14: SynchronizedBuffer.java
-// Creating a synchronized buffer using an ArrayBlockingQueue.
+
 import java.util.concurrent.ArrayBlockingQueue;
 
-public class BlockingBuffer implements Buffer {
-    private final ArrayBlockingQueue<Integer> buffer; // shared buffer
+public class BlockingBuffer {
+    // Shared buffer representing the checkout line
+    private final ArrayBlockingQueue<Integer> queue;
 
-    public BlockingBuffer() {
-        buffer = new ArrayBlockingQueue<Integer>(1);
+    public BlockingBuffer(int capacity) {
+        queue = new ArrayBlockingQueue<Integer>(capacity);
     }
 
-    // place value into buffer
-    @Override
-    public void blockingPut(int value) throws InterruptedException {
-        buffer.put(value); // place value in buffer
-        System.out.printf("%s%2d\t%s%d%n", "Producer writes ", value,
-                "Buffer cells occupied: ", buffer.size());
+    // Customer joins the queue
+    public void enqueueCustomer(int customerId) throws InterruptedException {
+        queue.put(customerId);
+        System.out.printf("Customer %2d joins the line.\tCustomers in queue: %d%n",
+                customerId, queue.size());
     }
 
-    // return value from buffer
-    @Override
-    public int blockingGet() throws InterruptedException {
-        int readValue = buffer.take(); // remove value from buffer
-        System.out.printf("%s %2d\t%s%d%n", "Consumer reads ",
-                readValue, "Buffer cells occupied: ", buffer.size());
-
-        return readValue;
+    // Cashier serves the customer
+    public int serveCustomer() throws InterruptedException {
+        int customerId = queue.take();
+        System.out.printf("Cashier serves Customer %2d.\tCustomers in queue: %d%n",
+                customerId, queue.size());
+        return customerId;
     }
 }
-
-
-
-/**************************************************************************
- * (C) Copyright 1992-2015 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
